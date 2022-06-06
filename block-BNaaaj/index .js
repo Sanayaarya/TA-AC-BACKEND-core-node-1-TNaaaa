305368f99226b2a6f57b0f3c689e0e8035f13d9d
Sanaya Arya <sanayaarya0123@gmail.com>
2022-06-06 16:32:49 +0530

    assignment done

--- block-BNaaaj/index .js
+++ block-BNaaaj/index .js
@@ -3,21 +3,28 @@ var os = require('os');
 var freeM = os.freemem();
 var cups = os.cpus().length;
 var uptime = os.uptime();
-var version = os.version();
-console.log(freeM);
-console.log(cpus);
-console.log(uptime);
-console.log(version);
+console.log(freeM, cpus,uptime);
 
-var readFiles = require('fs');
-readFiles.readFile('', 'utf8', (err, content) => {
-  console.log(content);
-});
 
-console.log('non-blocking First');
-setTimeout(() => {
-  console.log('non-blocking middle');
-}, 2000);
-console.log('last');
-var { readFile, readFileSync, unlike, fstat } = require('fs');
+var fs = require('fs');
+
+var {readFile, readFileSync, unLike}= require('fs');
+readFileSync('./app.js')
+
 var sync = readFileSync('./app.js');
+readFile('./app.js',(err,content)=>{
+  console.log(err, content)
+})
+ 
+
+// readFiles.readFile('', 'utf8', (err, content) => {
+//   console.log(content);
+// });
+
+// console.log('non-blocking First');
+// setTimeout(() => {
+//   console.log('non-blocking middle');
+// }, 2000);
+// console.log('last');
+// var { readFile, readFileSync, unlike, fstat } = require('fs');
+// var sync = readFileSync('./app.js');
