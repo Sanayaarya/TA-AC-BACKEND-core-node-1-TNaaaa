1f9879156663e423ed971f630f37375439bedb23
Sanaya Arya <sanayaarya0123@gmail.com>
2022-06-08 11:37:34 +0530

    done

--- block-BNaaar/index.js
+++ block-BNaaar/index.js
@@ -1,20 +1,20 @@
 var http = require('http');
-var url = require('url');
-var server = http.createServer(handleServer);
+ 
+var server = http.createServer(handleRequest);
 function handleServer(req, res) {
-  var parseUrl = url.parse(req.url);
-  console.log(req.method, req.url, parseUrl.pathname);
 
   if (req.method === 'GET' && req.url === '/') {
-    res.write('Welcome to Homepage');
-    res.end();
-  } else if (req.method === 'GET' && req.url === '/about') {
-    res.writeHead(201, { 'Content-type': 'text/html' });
+    res.setHeader('Content-type' , 'text/plain');
+    res.end('Welcome to Homepage');
+  
+  } else if (req.method === 'GET' && req.url === '/about') {   
+    res.setHeader('Content-type', 'text/html' );
     res.end('<h2>this is all about NodeJS </h2>');
   } else if (req.method === 'POST' && req.url === '/about') {
+    res.setHeader('Content-type', 'application/json' ); 
     res.end(`{message: this is a post request}`);
   }
 }
-server.listen(3000, () => {
+server.listen(5000, () => {
   console.log('server is listening on port 5k');
 });
